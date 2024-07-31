2d98bbd20bbde0d0cf2e4361a86c6e55b341c073
sopyansauri <sopyansauri09@gmail.com>
2024-07-31 16:29:18 +0700

    ubah judul

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,7 +13,7 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px text-lg sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('JURIG') }}
                     </x-nav-link>
                 </div>
 
